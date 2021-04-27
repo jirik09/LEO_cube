39e2913c77e4391df2f02f8a665fc57c4c80b900
HeyBirdie <jan.mucala@gmail.com>
2021-04-28 01:02:28 +0200

    update

--- Projects/F303RE_LEO_cube/Inc/usart.h
+++ Projects/F303RE_LEO_cube/Inc/usart.h
@@ -46,8 +46,8 @@ extern UART_HandleTypeDef huart2;
 uint8_t isXferComplete(void);
 uint16_t GetDMAIndex(void);
 void MX_UART_Init(void);
-uint8_t UARTsendChar(char chr);
-uint8_t UARTsendBuff(char *chr, uint32_t len);
+uint8_t UARTsendChar(char chr)  __attribute__((section(".ccmram")));
+uint8_t UARTsendBuff(char *chr, uint32_t len)  __attribute__((section(".ccmram")));
 
 #ifdef __cplusplus
 }
